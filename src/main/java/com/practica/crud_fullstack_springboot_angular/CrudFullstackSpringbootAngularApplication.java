000204645f323b490cc57c202dd539bcfc79c1b0
yeriix <yerayannel333@gmail.com>
2026-08-19 17:25:36 +0200

    Initial Spring Boot project setup and base structure

---
+++ src/main/java/com/practica/crud_fullstack_springboot_angular/CrudFullstackSpringbootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.practica.crud_fullstack_springboot_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFullstackSpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFullstackSpringbootAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/crud_fullstack_springboot_angular/CrudFullstackSpringbootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.crud_fullstack_springboot_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullstackSpringbootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
