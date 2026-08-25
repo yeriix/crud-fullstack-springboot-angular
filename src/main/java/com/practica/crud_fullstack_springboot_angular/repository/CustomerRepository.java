5742b2bb26f203dcada7f3f8c4d888d4f9fdaa02
yeriix <yerayannel333@gmail.com>
2026-08-26 00:57:49 +0200

    Edit CustomerRepository interface and CustomerService CRUD contract

--- src/main/java/com/practica/crud_fullstack_springboot_angular/repository/CustomerRepository.java
+++ src/main/java/com/practica/crud_fullstack_springboot_angular/repository/CustomerRepository.java
@@ -1,4 +1,8 @@
 package com.practica.crud_fullstack_springboot_angular.repository;
 
-public interface CustomerRepository {
+import com.practica.crud_fullstack_springboot_angular.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository <Customer, Integer> {
+
 }
--- src/main/java/com/practica/crud_fullstack_springboot_angular/service/CustomerService.java
+++ src/main/java/com/practica/crud_fullstack_springboot_angular/service/CustomerService.java
@@ -1,4 +1,13 @@
 package com.practica.crud_fullstack_springboot_angular.service;
 
+import com.practica.crud_fullstack_springboot_angular.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+    Customer save (Customer customer);
+    List<Customer> findAll();
+    Customer findById (Integer id);
+    void deleteById(Integer id);
+    Customer update(Customer customer);
 }
