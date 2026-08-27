927b2ecdd79d9726da6533855119184edbf0e3bc
yeriix <yerayannel333@gmail.com>
2026-08-27 05:31:39 +0200

    Implement customer creation flow with ngModel binding and CustomerService subscription

--- frontend-angular/src/app/components/customer-add/customer-add.ts
+++ frontend-angular/src/app/components/customer-add/customer-add.ts
@@ -1,19 +1,36 @@
 import {Component, OnInit} from '@angular/core';
+import {Customer} from '../../customer';
+import {FormsModule} from '@angular/forms';
+import {CustomerService} from '../../service/customer.service';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-customer-add',
   styleUrl: './customer-add.css',
   templateUrl: './customer-add.html',
 })
 export class CustomerAdd implements OnInit{
-  id : string='';
+  id : number= 0;
   firstName : string='';
   lastName : string='';
   email: string='';
 
+  constructor(private customerService : CustomerService) {
+
+  }
 
   ngOnInit(): void {
+  }
 
-    }
+  addCustomer() {
+    // Pasa null en lugar de 0 para que JPA/MySQL autogenere el ID
+    let customer = new Customer(null, this.firstName, this.lastName, this.email);
+    console.log(customer);
+
+    this.customerService.createCustomer(customer).subscribe({
+      next: (res) => console.log('Cliente guardado:', res),
+      error: (err) => console.error('Error desde el servidor:', err)
+    });
+  }
+
 }
--- frontend-angular/src/app/customer.ts
+++ frontend-angular/src/app/customer.ts
@@ -1,6 +1,6 @@
 export class Customer {
-  constructor(public id :number,
-              public firstName : string,
-              public lastName : string,
-              public email : string) {}
+  constructor(public id: number | null,
+              public firstName: string,
+              public lastName: string,
+              public email: string) {}
 }
